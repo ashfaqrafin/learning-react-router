3392433da042b3e951acce4f8c7fd33d8cc6b32c
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-06-15 07:13:56 +0600

    learned the use of loader

---
+++ src/pages/careers/Careers.jsx
@@ -0,0 +1,24 @@
+import { useLoaderData, Link } from "react-router-dom";
+
+const Careers = () => {
+  const careers = useLoaderData();
+
+  return (
+    <div className="careers">
+      {careers.map((career) => {
+        return (
+          <Link to="/" key={career.id}>
+            <p>{career.title}</p>
+            <p>Based in {career.location}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+export const careersLoader = async () => {
+  const response = await fetch("http://localhost:4000/careers");
+  return response.json();
+};
+
+export default Careers;
