a81f5cb6f4509b567b9c39b46d593f44e802c641
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-06-15 20:00:56 +0600

    learned how to handle forms

--- src/pages/help/Contact.jsx
+++ src/pages/help/Contact.jsx
@@ -1,8 +1,11 @@
+import { Form, redirect, useActionData } from "react-router-dom";
 const Contact = () => {
+  const data = useActionData();
+
   return (
     <div className="contact">
       <h3>Contact Us</h3>
-      <form>
+      <Form method="post" action="/help/contact">
         <label>
           <span>Your email:</span>
           <input type="email" name="email" required />
@@ -12,9 +15,23 @@ const Contact = () => {
           <textarea name="message" required></textarea>
         </label>
         <button>Submit</button>
-      </form>
+        {data && data.error && <p className="error">{data.error}</p>}
+      </Form>
     </div>
   );
+};
+
+export const contactAction = async ({ request }) => {
+  const data = await request.formData();
+  const submission = {
+    email: data.get("email"),
+    message: data.get("message"),
+  };
+
+  if (submission.message.length < 10) {
+    return { error: "Message too short" };
+  }
+  return redirect("/");
 };
 
 export default Contact;
